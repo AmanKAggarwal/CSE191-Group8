4b599f3848cc32e3605e4811abaca2a7b5e9511c
Group 08 <group08@cse191.com>
2022-06-04 16:35:15 -0700

    Backup everything

--- api/api.py
+++ api/api.py
@@ -11,6 +11,11 @@ from pyramid.response import Response
 import json
 from datetime import datetime
 
+#
+# Constants
+#
+DAY_VIEW_DATA_CSV_PATH = '/home/group08/api/csvs/day_view_data.csv'
+
 def get_data(query):
     conn = pymysql.connect(
         db='cse191',
@@ -22,7 +27,7 @@ def get_data(query):
     try:
         # read all the gateways that have data
         results = pd.read_sql(query, con=conn)
-        print(results)
+        # print(results)
     except:
         print("Unexpected exception occurred: ", sys.exc_info())
 
@@ -43,7 +48,7 @@ def put_data(query):
         # read all the gateways that have data
         cursor.execute(query)
         conn.commit()
-        print(query)
+        # print(query)
         result = "Insertion success"
     except:
         print("Unexpected exception occurred: ", sys.exc_info())
@@ -139,11 +144,11 @@ def log_devices(request):
         if request.params.has_key('color'):
             color = request.params['color']
 
-        print("groupname:",gn)
-        print("mac:",mac)
-        print("rssi:",rssi)
-        print("long:", long)
-        print("lang:", lang)
+        # print("groupname:",gn)
+        # print("mac:",mac)
+        # print("rssi:",rssi)
+        # print("long:", long)
+        # print("lang:", lang)
         
         locationStr = ''
         if long and lang:
@@ -158,12 +163,12 @@ UPDATE devices
 SET lastseen_ts = '{datetime.now()}', last_rssi = '{rssi}'{locationStr}{colorStr}
 WHERE groupname = '{gn}' AND mac = '{mac}';
         """
-        print(query)
+        # print(query)
     else:
         query = "SELECT * FROM devices;"
 
     dev = put_data(query)
-    print(dev)
+    # print(dev)
     # result = dev.to_json(orient="records")
     resp = Response('{ "devices":'+dev+'}')
     resp.content_type = 'application/json'
@@ -203,6 +208,65 @@ def log_beacon(request):
         return resp
 
 
+def fetch_day_view_data():
+    print("Fetching day view data")
+    query = """
+select dev.mac, dev.location, ble_logs.ble_mac, ble_logs.log_ts as total from (
+	select * from cse191.ble_logs
+    where ble_rssi > -70
+) as ble_logs
+inner join (
+	select * from cse191.devices
+    where mac in ('3C:71:BF:6C:5E:DC', '80:7D:3A:BB:06:40', 'C4:4F:33:0E:42:65', 'CC:50:E3:B0:21:94')
+) as dev
+on dev.mac = ble_logs.device_mac and dev.location is not null;
+"""
+    result = get_data(query)
+    times = pd.to_datetime(result.total)
+    grouped_data = result.groupby(['location', times.dt.hour])['ble_mac'].nunique()
+
+    grouped_data.to_csv(DAY_VIEW_DATA_CSV_PATH, sep=',', encoding='utf-8')
+
+def dayViewData(request):
+    try:
+        if not os.path.exists(DAY_VIEW_DATA_CSV_PATH):
+            fetch_day_view_data()
+        df = pd.read_csv(DAY_VIEW_DATA_CSV_PATH, sep=',', encoding='utf-8')
+
+        response = Response(df.to_json())
+        response.content_type = 'application/json'
+        response.headers.update({
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
+            'Access-Control-Allow-Headers': '*,Origin, Content-Type, Accept, Authorization',
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Max-Age': '1728000',
+            })
+        return response
+    except:
+        print("Unexpected exception occurred: ", sys.exc_info())
+    return None
+
+def getCountOfDates(request):
+    result = "none"
+    try:
+        results = pd.read_csv('/home/group08/api/csvs/file.csv')
+        print(results)
+        result = results.to_json()
+        resp = Response(result)
+        resp.content_type = 'application/json'
+        resp.headers.update({
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST,GET,DELETE,PUT,OPTIONS',
+            'Access-Control-Allow-Headers': '*,Origin, Content-Type, Accept, Authorization',
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Max-Age': '1728000',
+            })
+        return resp
+    except:
+        print("Unexpected exception occurred: ", sys.exc_info())
+    return None
+
 if __name__ == '__main__':
     with Configurator() as config:
         config.include('pyramid_jinja2')
@@ -220,9 +284,15 @@ if __name__ == '__main__':
         config.add_route('logdevices', '/logdevices')
         config.add_view(log_devices, route_name='logdevices', renderer='json')
 
-        config.add_route('logBeacon', '/logbeacons')
-        config.add_view(log_beacon, route_name='logBeacon', renderer='json')
+        config.add_route('logBeacons', '/logBeacons')
+        config.add_view(log_beacon, route_name='logBeacons', renderer='json')
         
+        config.add_route('getCountOfDates', '/getCountOfDates')
+        config.add_view(getCountOfDates, route_name='getCountOfDates', renderer='json')
+        
+        config.add_route('dayViewData', 'dayViewData')
+        config.add_view(dayViewData, route_name='dayViewData', renderer='json')
+
         app = config.make_wsgi_app()
 
     print(datetime.now(), "Starting server...Group 08")
@@ -230,3 +300,4 @@ if __name__ == '__main__':
     #server = make_server('0.0.0.0', 8008, app)
     #server.serve_forever()
     serve(app, host='0.0.0.0', port='8008', threads=32)
+
--- webiot/g08iot.ino
+++ webiot/g08iot.ino
@@ -5,6 +5,11 @@
 
 // Global includes
 #include <ArduinoJson.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+
 
 // wifi comms includes
 #include <WiFi.h>
@@ -14,8 +19,9 @@
 // local includes
 
 // check device MAC table to get wifi password
-#define MAN_SSID "anadev"
-#define MAN_PSWD "123ana456"  
+
+#define MAN_SSID "UCSD-DEVICE"
+#define MAN_PSWD "Fj7UPsFHb84e"  
 
 // --------------------------------------------------------------------
 
@@ -24,7 +30,8 @@
 
 static volatile bool wifiConnected = false;
 String localSSID, localPSWD;
-String regURL;
+String logBeaconsURL;
+String logDeviceURL;
 String postStr;
 
 static volatile bool tog = false;
@@ -33,6 +40,47 @@ static volatile int userCount = 0;
 
 // -------------------------------------------------------
 
+
+int scanTime = 1; //In seconds
+BLEScan* pBLEScan;
+String becaonStrList = "";
+char buf[2000];
+
+StaticJsonDocument<4000> jBLEList;
+// create an empty array
+JsonArray bleArray = jBLEList.to<JsonArray>();
+
+String  parseBeacon(BLEAdvertisedDevice dev) {
+
+      // vars used in building json object
+      StaticJsonDocument<400> jBLEDoc;
+      char jChar[800] = "";
+
+      // clear the json object
+      jBLEDoc.clear();
+      
+      jBLEDoc["mac"] = String(dev.getAddress().toString().c_str());   // get the beacon mac addr
+      jBLEDoc["rssi"] = String(dev.getRSSI());                        // record rssi power
+     
+
+      serializeJson(jBLEDoc, jChar);                                  // convert to string
+      //Serial.printf("BLE JSON (%d): %s\n",jBLEDoc.memoryUsage(),jChar);
+      
+      if (becaonStrList.length() > 0)
+        becaonStrList = becaonStrList + "," + String(jChar);
+      else
+        becaonStrList = becaonStrList + String(jChar);
+      
+      return String(jChar);
+}
+
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+      parseBeacon(advertisedDevice);
+    }
+};
 
 
 /*********************** SETUP ******************************/
@@ -43,7 +91,8 @@ void setup(){
 
   delay(50);
   Serial.println("CSE191 IoT Web call example");
-  
+  Serial.println("CSE 191 Scanning Example");
+
   WiFi.onEvent(WiFiEvent);
   WiFi.mode(WIFI_MODE_STA);
 
@@ -68,7 +117,13 @@ void setup(){
   while (!wifiConnected)
       ; 
       
-  getGeoLocation();
+//  getGeoLocation();
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);  // less or equal setInterval value
 }
 
 // ------------------------------ MAIN LOOP --------------------------------
@@ -76,11 +131,24 @@ void loop(){
 
 /* From last LAB ../apixx/logdevices?gn=<groupname>&mac=<mac address>&rssi=<rssi value> */
 
-  regURL = "http://cse191.ucsd.edu/api08/logdevices?gn='innovators'&mac="+getMacStr()+"&rssi="+WiFi.RSSI();
-  getHTTP(regURL);
+  logDeviceURL = "http://cse191.ucsd.edu/api08/logdevices?gn=Innovators&mac="+getMacStr()+"&rssi="+WiFi.RSSI();
 
-  //postStr = "{\"field\": \"value\", \"other\": \"45\"}";
-  //postJsonHTTP(regURL, postStr);
+  Serial.println(logDeviceURL);
+  getHTTP(logDeviceURL);
+
+  becaonStrList = "";
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  Serial.print("Devices found: ");
+  Serial.println(foundDevices.getCount());
+  Serial.println("Scan done!");
+  becaonStrList = "{\"groupname\":\"Innovators\", \"dev_mac\":\""+getMacStr()+"\", \"dev_rssi\":\""+WiFi.RSSI()+"\", \"beacons\":[" + becaonStrList + "]}";
+  becaonStrList.toCharArray(buf, sizeof(buf));
+//  Serial.printf("BLE List: %s\n",buf);
+  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+
+  logBeaconsURL = "http://cse191.ucsd.edu/api08/logBeacons";
+  postJsonHTTP(logBeaconsURL, becaonStrList);
+  Serial.println("Beacons sent");
 
   delay(10000);
 
